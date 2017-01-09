806ca75eea8eaa90577e543e3899b59acbc0ae9b
RonDavidson <rondavidson@gmail.com>
2017-01-09 14:32:50 -0500

    Add comments and make lines shorter

--- media.py
+++ media.py
@@ -2,10 +2,13 @@ import webbrowser
 
 class Movie():
     def __init__(self, movie_title,movie_storyline,movie_image,movie_trailer):
+        '''take the movie title, storyline, poster url, and youtube trailer
+        link to create a class that we can use in a trailer website'''
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = movie_image
         self.trailer_youtube_url = movie_trailer
 
     def show_trailer(self):
+        '''open the youtube trailer link on the default browser'''
         webbrowser.open(self.trailer_youtube_url)
